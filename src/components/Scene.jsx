b7f330dd6f4647218e191c834ef22944c85b5f0f
Alaina H <97alaina@gmail.com>
2022-08-31 08:54:46 -0400

    deleted Scene

--- src/components/Scene.jsx
+++
@@ -1,8 +0,0 @@
-import Spline from '@splinetool/react-spline';
-import React from 'react';
-
-export default function Scene() {
-  return (
-    <Spline scene="https://prod.spline.design/X09PcItup65OEMQC/scene.splinecode" />
-  );
-}
