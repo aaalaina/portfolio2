fbac278bf4cc2a98e76ac4c9ff4038be6f565111
Alaina H <97alaina@gmail.com>
2022-09-01 15:06:40 -0400

    deleted unused imports

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
 import { Box, Button, styled, Typography } from "@mui/material";
 import React from "react";
-import { Link } from "react-scroll";
+
 import reactImg from "../media/react_svg.svg";
 
 const Hero = () => {
--- src/components/Stack.jsx
+++ src/components/Stack.jsx
@@ -1,7 +1,6 @@
-import { Typography, Box, styled } from "@mui/material";
+
 import React from "react";
-import { icons } from "../constants/icons";
-import Icon from "./Icon";
+
 
 
 
