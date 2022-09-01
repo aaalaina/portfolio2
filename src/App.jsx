3ea73eb2abfcb753c96cb4b5a37051fb6451e2b9
Alaina H <97alaina@gmail.com>
2022-09-01 15:09:14 -0400

    fixed minor component error

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import About from "./components/About";
 import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
-import Testimonial from "./components/Testimonial";
 import Work from "./components/Work";
 import Contact from './components/Contact';
 
